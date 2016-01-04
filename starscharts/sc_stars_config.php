8182f0d8e8865c574c7071bc6b1f09319ed1dfc2
dklugmann <david@myastrologycharts.com>
2016-01-04 14:59:32 +0000

    Stars Web Service
    Stars Web Service

---
+++ starscharts/sc_stars_config.php
@@ -0,0 +1,7 @@
+<?
+$apikey = '554c7d6caca52';
+$starspath = "http://www.myastrologycharts.com/astroservice/";
+$enginepath = "http://www.myastrologycharts.com/";
+$imagepath = "http://www.myastrologycharts.com/serviceimages";
+$enginename = "engineservice.php";
+?>
---
+++ starsreports/stars/stars_config.php
@@ -0,0 +1,6 @@
+<?
+$apikey = '554c7d6caca52';
+$starspath = "http://www.myastrologycharts.com/astroservice/";
+$enginepath = "http://www.myastrologycharts.com/";
+$enginename = "engineservice.php";
+?>
